3f56cd32858948143cf599d6b14305d831faa79e
katekk2003 <katejeffries200@gmail.com>
2025-03-17 21:10:20 -0600

    Initial Commit

---
+++ backend/BookstoreProject.API/Controllers/BookstoreController.cs
@@ -0,0 +1,23 @@
+﻿using BookstoreProject.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class BookstoreController : ControllerBase
+    {
+        private BookstoreDbContext _context;
+        public BookstoreController(BookstoreDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IEnumerable<Book> Get()
+        {
+            return _context.Books.ToList();
+        }
+    }
+}
---
+++ frontend/src/BookList.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { Book } from './types/Book';
+
+function BookList() {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const response = await fetch('https://localhost:5000/api/Bookstore');
+      const data = await response.json();
+      setBooks(data);
+    };
+
+    fetchBooks();
+  }, []);
+
+  return (
+    <>
+      <h1>Books</h1>
+      <br />
+      {books.map((b) => (
+        <div id="bookCard">
+          <h3>{b.title}</h3>
+          <ul>
+            <li>Author: {b.author}</li>
+            <li>Publisher: {b.publisher}</li>
+            <li>ISBN: {b.isbn}</li>
+            <li>Classification: {b.classification}</li>
+            <li>Category: {b.category}</li>
+            <li>Page Count: {b.pageCount}</li>
+            <li>Price: ${b.price}</li>
+          </ul>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default BookList;
